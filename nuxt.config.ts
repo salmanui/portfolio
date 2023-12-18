ab4eced7f8dcdddc1110321f3f59432a80abd896
salmanui <147985811+salmanui@users.noreply.github.com>
2023-12-18 15:10:15 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-   router:{
+  target: 'static',
+  router:{
     base:"/portfolio/"
   },
   postcss: {
